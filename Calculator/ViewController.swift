d3b25238cd93ded334b676276369c3e5cd5d8705
Omar Ashraf <amourakora2001@gmail.com>
2024-02-09 17:36:40 +0200

    implement "AC", "+/-", "%" calculator buttons, prevent inserting more than 1 decimal point "."

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,15 +12,56 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    var number = ""
-    var isFinishedTyping = true
+    private var isFinishedTyping = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTyping = true
         
-        //What should happen when a non-number button is pressed
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Failed to convert display label text to Double, possibly because it contains non-numerical characters")
+        }
+
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
+        
+        
+//        var number: Float {
+//            get   {
+//                var tempNumber: Float = 0.0
+//                if let pressedButton = sender.currentTitle {
+//                    switch pressedButton {
+//                    case "AC":
+//                        tempNumber = 0
+//                    case "+/-":
+////                        let x = 5.2
+////                        let y = Int(x)
+////                        print(y)
+//                        print(displayLabel.text!)
+//                        let displayLabelFloat = Float(displayLabel.text!)!
+//                        let displayLabelFloatInt = Int(displayLabelFloat)
+//                        let displayLabelFloatIntFloat = Float(displayLabelFloat)
+//                        tempNumber = -displayLabelFloat
+//                    case "%":
+//                        tempNumber = Float(displayLabel.text!)! / 100
+//                    default:
+//                        tempNumber = 0
+//                    }
+//                }
+//                return tempNumber
+//            }
+//
+//        }
+//        displayLabel.text = "\(number)"
         
     }
-    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
@@ -31,11 +72,21 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTyping = false
             } else {
+                if numValue == "." {
+                    
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Failed to convert display label text to Double, possibly because it contains non-numerical characters")
+                    }
+                    // Quick way to initialize boolean
+                    let isInt = (floor(currentDisplayValue) == currentDisplayValue)
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
                 displayLabel.text! += numValue
             }
         }
-        
     }
-    
 }
-
